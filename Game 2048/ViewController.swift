6df6037452d86cfd0a0021acfd5aa44d68a7fd7e
Alex Danilin <alex.danilin.ad@gmail.com>
2017-10-07 13:49:41 +0300

    Project init

---
+++ Game 2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game 2048
+//
+//  Created by Alexander Danilin on 07/10/2017.
+//  Copyright © 2017 Alexander Danilin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
