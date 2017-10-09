6a0f2b41272bd4418be1c6adee3fbfbb2730aa9d
Alex Danilin <alex.danilin.ad@gmail.com>
2017-10-09 19:06:24 +0300

    Board Engine first alpha

--- Game 2048/MainVC.swift
+++ Game 2048/MainVC.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class MainVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    let board = Board()
+    
+    @IBAction func swipe(recognizer:UIGestureRecognizer?) {
+        guard let recognizer = recognizer as? UISwipeGestureRecognizer else {
+            return
+        }
+        switch recognizer.direction {
+        case UISwipeGestureRecognizerDirection.right:
+            board.moveTile(direction: .forward, orientation: .horizon)
+        case UISwipeGestureRecognizerDirection.left:
+            board.moveTile(direction: .backward, orientation: .horizon)
+        case UISwipeGestureRecognizerDirection.up:
+            board.moveTile(direction: .backward, orientation: .vertical)
+        case UISwipeGestureRecognizerDirection.down:
+            board.moveTile(direction: .forward, orientation: .vertical)
+        default:
+            break
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLoad() {
+        if let view = self.view {
+            view.backgroundColor = UIColor(white: 1.0, alpha: 1)
+            view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+            
+            for direction : UISwipeGestureRecognizerDirection in [.left, .right, .up, .down] {
+                let gesture = UISwipeGestureRecognizer(target: self, action: #selector(swipe))
+                gesture.direction = direction
+                view.addGestureRecognizer(gesture)
+            }
+            
+            board.addTo(view: view)
+            board.buildBoard()
+        }
     }
 }
 
